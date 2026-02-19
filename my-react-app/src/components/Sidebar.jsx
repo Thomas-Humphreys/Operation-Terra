9fa492eea1d65af52afc75b562c031ebe5ad3d97
Thomas Humphreys <thomaswhumphreys25@gmail.com>
2026-02-19 18:58:49 +0000

    updating baseline + sidebar start

---
+++ my-react-app/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import "../Styles/sidebar.css";
+function Sidebar() {
+    return(
+        <aside className="sideBarContainer">
+            <p>hi</p>
+        </aside>
+    )
+
+}
+
+export default Sidebar;
