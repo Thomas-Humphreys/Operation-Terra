db641d259f570ce77838d4d0f58460d6c0d60099
Thomas Humphreys <thomaswhumphreys25@gmail.com>
2026-02-19 19:55:24 +0000

    changed to grid

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,7 +3,13 @@ import MainContainer from './components/MainContainer'
 
 function App() {
     return(
-      <MainContainer />
+      //<MainContainer />
+      <div className="parent">
+          <div className="sidebar"> </div>
+          <div className="chatContainer"> </div>
+          <div className="terraImageContainer"> </div>
+          <div className="chatWindow"> </div>
+      </div> 
     )
 }
 
