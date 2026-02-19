fb1c1968c1c7861736e902e46071446e6490d340
Thomas Humphreys <thomaswhumphreys25@gmail.com>
2026-02-19 19:23:38 +0000

    basic layout established

--- my-react-app/src/components/MainContainer.jsx
+++ my-react-app/src/components/MainContainer.jsx
@@ -1,10 +1,12 @@
 import Sidebar from "./Sidebar";
 import "../Styles/containers.css";
+import ChatContainer from "./ChatContainer";
 
 function MainContainer(){
     return(
         <main className="mainContainer">
             <Sidebar /> 
+            <ChatContainer />
         </main>
     )
 }
