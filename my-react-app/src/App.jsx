e5b877ab95c3995c3cb616a2f998de0f91b9bd6b
Thomas Humphreys <thomaswhumphreys25@gmail.com>
2026-02-19 20:00:57 +0000

    notes on todo

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,12 +5,21 @@ function App() {
     return(
       //<MainContainer />
       <div className="parent">
-          <div className="sidebar"> </div>
-          <div className="chatContainer"> </div>
-          <div className="terraImageContainer"> </div>
-          <div className="chatWindow"> </div>
+          <div className="sidebar">sidebar</div>
+          <div className="chatContainer">typing container</div>
+          <div className="terraImageContainer">terra image</div>
+          <div className="chatWindow"> chat window </div>
       </div> 
     )
 }
 
-export default App
+export default App;
+/*
+- figuring out design + overall layout
+- finish basic frontend
+- make python api thing
+- connect ai to the frontend
+- make it pretty
+- additiona features and testing
+
+*/
