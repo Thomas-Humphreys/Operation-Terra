aa7cd0e3707b8079a181e3b35166fef1d97d0b18
JahnaviC2021 <jchalla2021@gmail.com>
2026-02-22 05:51:08 +0000

    Continuing text area prompt and added icons from lucide-react

--- my-react-app/src/components/TypingArea.jsx
+++ my-react-app/src/components/TypingArea.jsx
@@ -1,21 +1,35 @@
 import "../Styles/typingArea.css";
-
+import { Search, AudioLines, Plus } from "lucide-react";
+import { useState } from "react";
 
 function TypingArea(){
+    const [prompt, setPrompt] = useState("");
+
     return(
         // <p>typing things here</p>
         <div className="textArea">
+            
             {/* text area */}
-            <div className="textInput">
-
+            <div className="textContainer"> 
+                {/* <div className="textForm"> type here</div> */}
+                {/* redo textarea!!!!!!!!! */}
+                <textarea type="text" rows="5" cols="120" placeholder="prompt" value={prompt} onChange={(e) => setPrompt(e.target.value)}/>
             </div>
+      
 
             <div className="searchBar">
-                <div className="addDocument searchBarButton"> 1 </div>
-                <div>empty </div>
-                <div className="audio searchBarButton"> 2 </div>
-                <div className="submitText searchBarButton"> 3 </div>
+                <div className="addDocument searchBarButton"><Plus />
+                    <span className="tooltiptext">Add Document</span>
+                </div>
+                <div /> {/* for empty space */}
+                <div className="audio searchBarButton"><AudioLines />
+                    <span className="tooltiptext">Audio</span>
+                </div>
+                <div className="submitText searchBarButton"><Search />
+                    <span className="tooltiptext">Submit</span>
+                </div>
             </div>
+            {/* </div> */}
         </div>
     )
 }
