7909b8d17c49ca0f60776fec382641dc67206523
Thomas Humphreys <thomaswhumphreys25@gmail.com>
2026-02-22 22:10:10 +0000

    basic start on sidebar

--- my-react-app/src/components/Sidebar.jsx
+++ my-react-app/src/components/Sidebar.jsx
@@ -1,9 +1,31 @@
 import "../Styles/sidebar.css";
 function Sidebar() {
+    const Quests =[
+        {
+            id: 0,
+            questName: "Recycle",
+            questDescription: "blah blah",
+            questWinLossPoints: 20,
+            currentStatus: "Ongoing"
+        },
+         {
+            id: 1,
+            questName: "Learn about deforistation",
+            questDescription: "blah blah",
+            questWinLossPoints: 40,
+            currentStatus: "Complete"
+        }
+    ]
+    // current status can be ongoing, failed, or completed.
+
     return(
-            <p>hi</p>
+            <div className="CardContainer">
+                {}
+            </div>
     )
 
 }
 
 export default Sidebar;
+
+// add in settings 
