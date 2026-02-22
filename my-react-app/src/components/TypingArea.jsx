d401df6670859be228dec53811467b998a8f9df2
JahnaviC2021 <jchalla2021@gmail.com>
2026-02-22 03:49:38 +0000

    Started chat box, still incomplete.

--- my-react-app/src/components/TypingArea.jsx
+++ my-react-app/src/components/TypingArea.jsx
@@ -1,7 +1,22 @@
 import "../Styles/typingArea.css";
+
+
 function TypingArea(){
     return(
-        <p>typing things here</p>
+        // <p>typing things here</p>
+        <div className="textArea">
+            {/* text area */}
+            <div className="textInput">
+
+            </div>
+
+            <div className="searchBar">
+                <div className="addDocument searchBarButton"> 1 </div>
+                <div>empty </div>
+                <div className="audio searchBarButton"> 2 </div>
+                <div className="submitText searchBarButton"> 3 </div>
+            </div>
+        </div>
     )
 }
 
