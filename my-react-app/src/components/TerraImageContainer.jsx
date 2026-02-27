2ba9f12610450dfa663a6eadca2393c026c47d06
JahnaviC2021 <jchalla2021@gmail.com>
2026-02-27 00:26:02 +0000

    terra added

--- my-react-app/src/components/TerraImageContainer.jsx
+++ my-react-app/src/components/TerraImageContainer.jsx
@@ -1,7 +1,53 @@
+import happyTerra from '../assets/happy.png';
+import sadTerra from '../assets/sad.png';
+import neutralTerra from '../assets/neutral.png';
+import angryTerra from '../assets/angry.png';
+import { Laugh, Frown, Meh, Angry } from "lucide-react";
 
-function TerraImageContainer(){
+import "../Styles/terraImageContainer.css";
+function TerraImageContainer({ terraMood = "angry" }){
+
+    let terraImage;
+
+    switch(terraMood){
+        case "happy":
+            terraImage = happyTerra;
+            break;
+        case "sad":
+            terraImage = sadTerra;
+            break;
+        case "neutral":
+            terraImage = neutralTerra;
+            break;
+        case "angry":
+            terraImage = angryTerra;
+            break;
+        default:
+            terraImage = neutralTerra;
+    }
+
+    const moods = [
+        { name: "happy", icon: <Laugh />, color: "#c6d448" },
+        { name: "neutral", icon: <Meh />, color: "#48d472" },
+        { name: "sad", icon: <Frown />, color: "#48b1d4" },
+        { name: "angry", icon: <Angry />, color: "#d46448" }
+    ];
+    
     return(
-        <img id="terraImage"></img>
+        <div className="terraImageContainer">
+            <img id="terraImage" src={angryTerra} alt="Happy Terra" width="350px" height="450px" />
+            <div className="moodIndicatorBar">
+                {moods.map((mood) => (
+                    <div 
+                        key={mood.name}
+                        className={`moodButton ${mood.name} ${terraMood === mood.name ? 'active' : ''} ${terraMood === "angry" && mood.name === "angry" ? 'angryGlow' : ''} `}
+                        style={{ backgroundColor: terraMood === mood.name ? mood.color : 'transparent' }}
+                    >
+                        {mood.icon}
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
